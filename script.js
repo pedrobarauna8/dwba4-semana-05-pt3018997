bdda2036047a53eaa0845d291827614d827569a3
pedrobarauna8 <81633121+pedrobarauna8@users.noreply.github.com>
2023-08-25 00:33:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function inserirMenu() {
+  fetch('Menu.html')
+    .then(response => response.text())
+    .then(menuHtml => {
+      const menuContainer = document.getElementById('menu_container');
+      menuContainer.innerHTML = menuHtml;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', inserirMenu);
